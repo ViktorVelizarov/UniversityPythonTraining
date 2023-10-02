f344f12e8da49323ac29f5bad4e9e7c21a14549d
ViktorVelizarov <58163160+ViktorVelizarov@users.noreply.github.com>
2023-10-02 15:36:26 +0200

    create a function that returns current time and output it in html

--- week5/FlaskExcersises/TimeAndLight/app.py
+++ week5/FlaskExcersises/TimeAndLight/app.py
@@ -1,12 +1,24 @@
 from flask import Flask
 from flask import render_template
+from datetime import datetime
+
+
+def currentTime():
+    rightNow = datetime.now()
+    time = rightNow.strftime("%I:%M%p")
+    time = time.lstrip('0')
+    time = time.lower()
+    day = rightNow.strftime("%A")
+
+    return "It is " + time + " on " + day + "."
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    return render_template('index.html', time=currentTime())
 
 
 app.run()
