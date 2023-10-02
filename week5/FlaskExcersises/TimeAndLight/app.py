9ceae2505df8be6689f0597ef011b1e2bb20664e
ViktorVelizarov <58163160+ViktorVelizarov@users.noreply.github.com>
2023-10-02 19:05:34 +0200

    turn the data into a list and show the most recent 10 entries in html

--- week5/FlaskExcersises/TimeAndLight/app.py
+++ week5/FlaskExcersises/TimeAndLight/app.py
@@ -3,6 +3,8 @@ from flask import render_template
 from datetime import datetime
 import random
 
+listOfMeasurments = []
+
 
 def currentTime():
     rightNow = datetime.now()
@@ -20,11 +22,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    global listOfMeasurments
     random_color = "#{:02x}{:02x}{:02x}".format(random.randint(
         0, 255), random.randint(0, 255), random.randint(0, 255))
-
-    return render_template('index.html', time=currentTime(),
-                           light=currentLight(),  random_color=random_color)
+    listOfMeasurments.insert(0, (currentTime(), currentLight(), random_color))
+    listOfMeasurments = listOfMeasurments[:10]
+    return render_template('index.html', measurments=listOfMeasurments)
 
 
 app.run()
